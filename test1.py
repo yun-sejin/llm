c262e7a4b3dd342cfc944162ee18465e29efec60
yun-sejin <luemier@hanmail.net>
2024-06-26 07:30:36 +0900

    fetch all 수정

--- test1.py
+++ test1.py
@@ -18,20 +18,37 @@ def upsert_all_data():
     # cur = conn.cursor()
     
     # Fetch file_list from the dsllm_job_hist table
-    # cur.execute("SELECT file_list FROM dsllm_job_hist")
-    # file_list = cur.fetchall()  # Fetch all rows
+    #cur = conn.cursor()
 
-    file_list = [['20240626/aaa/qna.json','20240626/aaa/qna3.json'], ['20240626/bbb/qna2.json','20240626/bbb/qna3.json']]
+    # SQL query to select job_id and file_list where job_type is 'upload' and success_yn is 'Y'
+    # query = """
+    # SELECT job_id, file_list FROM dsllm_job_hist
+    # WHERE job_type = 'upload' AND success_yn = 'Y'
+    # """
+    # cur.execute(query)
 
-    for sub_list in file_list:
+    # # Fetch all matching rows
+    # rows = cur.fetchall()
+
+    # for row in rows:
+    #     print(row)  # Each 'row' is a tuple with (job_id, file_list)
+
+    #file_list = [['20240626/aaa/qna.json','20240626/aaa/qna3.json'], ['20240626/bbb/qna2.json','20240626/bbb/qna3.json']]
+    file_list = (('123345', ['20240626/aaa/qna.json','20240626/aaa/qna3.json']), ('765432',['20240626/bbb/qna2.json','20240626/bbb/qna3.json']))
+    rows = (('123345', ['20240626/aaa/qna.json','20240626/aaa/qna3.json']), ('765432',['20240626/bbb/qna2.json','20240626/bbb/qna3.json']))
+    # for sub_list in file_list:
+    for row in rows:
+        print(row)
+        file_list0 = row[0]
+        file_list = row[1]
         all_data = []
-        for file_path in sub_list:
+        for file_path in file_list:
             with open(f"{base_dir2}/{file_path}", 'r') as f:
                 data = json.load(f)
                 all_data.append(data)
         print(all_data)        
         #all_data = [{'id': '345', 'job_id': '12345', 'sub_id': '5678', 'all': {'question': 'What is the capital of France?', 'answer': 'seoul'}}, {'id': '345', 'job_id': '12345', 'sub_id': '5678', 'all': {'question': 'What is the capital of France?', 'answer': 'seoul'}}]
-        data_list = []
+        data_list = ()
         for data in all_data:
             uuid_id = uuid.uuid4()
             id = data["id"]
